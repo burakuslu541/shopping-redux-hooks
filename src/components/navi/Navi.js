12621b617e35420c1e0f4e8617a2884fd6d05de4
burak <burakuslu541@gmail.com>
2021-01-09 20:28:21 +0300

    first commit

---
+++ src/components/navi/Navi.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from "reactstrap";
+import CartSummary from "../cart/CartSummary";
+
+export default class Navi extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand><Link to="/">Nortwind Mağazası</Link></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink><Link to="/saveproduct">Ürün Ekle</Link></NavLink>
+              </NavItem>
+              <CartSummary/>
+            </Nav>
+            <NavbarText>Simple Text</NavbarText>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
